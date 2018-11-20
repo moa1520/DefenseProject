d5d0dbe2c6b5a37b612b2c497efc755427de5896
KangTaekyung <douit_be@naver.com>
2018-11-20 19:11:03 +0900

    타이틀프래그먼트 수정

--- app/src/main/java/com/example/taekyoungkang/defenseproject/TitlesFragment.java
+++ app/src/main/java/com/example/taekyoungkang/defenseproject/TitlesFragment.java
@@ -1,5 +1,6 @@
 package com.example.taekyoungkang.defenseproject;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +18,6 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class TitlesFragment extends Fragment {
-    MyAdapter adapter;
     public TitlesFragment() {
         // Required empty public constructor
     }
@@ -24,24 +26,37 @@ public class TitlesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        ArrayList<MyItem> data= new ArrayList<MyItem>();
-        data.add(new MyItem(R.drawable.s1,"설악산 수렴동대피소","주상현","강원 인제군 북면 백담로 1220"));
-        data.add(new MyItem(R.drawable.s2,"지리산 세석동대피소","강태경","경남 산청군 시천면 내대리"));
-        data.add(new MyItem(R.drawable.s3,"덕유산 향적봉대피소","배장근","전북 무주군 설천면 삼공리 산109"));
-        data.add(new MyItem(R.drawable.s4,"지리산 노고단대피소","박종범","전남 구례군 산동면 노고단로 1068"));
 
         View rootView = (View)inflater.inflate(R.layout.fragment_titles, container, false);
-        ListView lv = (ListView) rootView.findViewById(R.id.listview);
-
-        adapter = new MyAdapter(getActivity(), R.layout.info, data);
-
-        lv.setAdapter(adapter);
-
-        lv.setDivider(new ColorDrawable(Color.rgb(200,200,200)));
-        lv.setDividerHeight(5);
-
+        ListView lv = (ListView)rootView.findViewById(R.id.listview);
+        lv.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, Defenseinfo.TITLES));
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            }
+//        });
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         return rootView;
+
+        // Inflate the layout for this fragment
+//        ArrayList<MyItem> data= new ArrayList<MyItem>();
+//        data.add(new MyItem(R.drawable.s1,"설악산 수렴동대피소","주상현","강원 인제군 북면 백담로 1220"));
+//        data.add(new MyItem(R.drawable.s2,"지리산 세석동대피소","강태경","경남 산청군 시천면 내대리"));
+//        data.add(new MyItem(R.drawable.s3,"덕유산 향적봉대피소","배장근","전북 무주군 설천면 삼공리 산109"));
+//        data.add(new MyItem(R.drawable.s4,"지리산 노고단대피소","박종범","전남 구례군 산동면 노고단로 1068"));
+//
+//        View rootView = (View)inflater.inflate(R.layout.fragment_titles, container, false);
+//        ListView lv = (ListView) rootView.findViewById(R.id.listview);
+//
+//        adapter = new MyAdapter(getActivity(), R.layout.info, data);
+//
+//        lv.setAdapter(adapter);
+//
+//        lv.setDivider(new ColorDrawable(Color.rgb(200,200,200)));
+//        lv.setDividerHeight(5);
+//
+//        return rootView;
+//        return inflater.inflate(R.layout.fragment_titles, container, false);
     }
 
 }
